e0a9b8c80315f7ff34ca7c6eba38c7b9e9f8cc4e
Bless Ziamah <tziamah1@gmail.com>
2024-03-08 23:49:51 +0000

    Home Left sidebar

--- frontend/job-portal/src/sidebar/Location.jsx
+++ frontend/job-portal/src/sidebar/Location.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import "../App.css"
+import InputField from "../components/InputField.jsx";
 
-const Location = () => {
+const Location = ({handleChange}) => {
 	return (
 		<div>
 			<h4 className={"text-lg font-medium mb-2"}>Location</h4>
 
 			<div>
 				<label className={"sidebar-label-container"}>
-					<input type={"radio"} name={"test"} id={"test"} />
+					<input type={"radio"} name={"test"} id={"test"} value={""} onChange={handleChange}/>
+					<span className={"checkmark"}></span>All
 				</label>
+
+				<InputField handleChange={handleChange} name={"test"} value={"London"} title={"London"}/>
+				<InputField handleChange={handleChange} name={"test"} value={"Seattle"} title={"Seattle"}/>
+				<InputField handleChange={handleChange} name={"test"} value={"Madrid"} title={"Madrid"}/>
+				<InputField handleChange={handleChange} name={"test"} value={"Boston"} title={"Boston"}/>
 			</div>
 		</div>
 	);
--- frontend/job-portal/src/sidebar/Sidebar.jsx
+++ frontend/job-portal/src/sidebar/Sidebar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Location from "./Location.jsx";
 
 const Sidebar = ({handleChange, handleClick}) => {
 	return (
 		<div className={"space-y-5"}>
 			<h3 className={"text-lg font-bold mb-2"}>Filters</h3>
 
-			<Location handleChange={handleChange}/>
+			<Location handleChange={handleChange} />
 		</div>
 	);
 };
