228b35c67218fc63a1c7df999ce0daf203c6926d
Bless Ziamah <tziamah1@gmail.com>
2024-03-08 15:14:11 +0000

    Nav bar

---
+++ frontend/job-portal/src/router/Router.jsx
@@ -0,0 +1,16 @@
+import {createBrowserRouter} from "react-router-dom";
+import App from "../App.jsx";
+import Home from "../pages/Home.jsx";
+import About from "../pages/About.jsx";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+		children: [
+			{path: "/", element: <Home />},
+		]
+	}
+])
+
+export default router
