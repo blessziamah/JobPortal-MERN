9c12dad2365569ab422dd1d1289ee9a23cba6c45
Bless Ziamah <tziamah1@gmail.com>
2024-03-10 18:29:32 +0000

    Backend CRUD API created

--- backend/routes/jobsRoute.js
+++ backend/routes/jobsRoute.js
@@ -59,12 +59,14 @@ router.post("/", async (req, res) => {
 // Get book with a particular ID
 router.get("/:id", async (req, res) => {
 	try{
-		const jobs = await Job.find({})
+		const {id} = req.params
+
+		const job = await Job.findById(id)
 
 		res.status(200).json({
-			data: jobs
+			data: job
 		})
-		console.log(jobs)
+		console.log(job)
 	} catch (error) {
 		console.log(error)
 		res.status(500).send({message: error.message})
@@ -72,4 +74,56 @@ router.get("/:id", async (req, res) => {
 
 })
 
+
+// Update book with a particular ID
+router.put("/:id", async (req, res) => {
+	try {
+		if(
+			!req.body.companyName || !req.body.jobTitle ||
+			!req.body.minPrice || !req.body.maxPrice ||
+			!req.body.salaryType || !req.body.jobLocation ||
+			!req.body.postingDate || !req.body.experienceLevel ||
+			!req.body.employmentType || !req.body.description
+		) {
+			res.status(500).send("All fields are required")
+		}
+		const {id} = req.params
+
+
+
+		const updatedJob = await Job.findByIdAndUpdate(id, req.body)
+
+		if(!updatedJob) {
+			res.status(404).send({message: "No job with the above ID was found"})
+			console.log("No job with the above ID was found")
+		}
+
+		return res.status(200).send({message: "Book updated successfully."})
+	} catch (error) {
+		console.log(error)
+		res.status(500).send("An error occurred")
+	}
+})
+
+
+// Delete job with specific ID
+router.delete("/:id", async (req, res) => {
+	try {
+		const {id} = req.params
+
+
+
+		const result = await Job.findByIdAndDelete(id, req.body)
+
+		if(!result) {
+			res.status(404).send({message: "No job with the above ID was found"})
+			console.log("No job with the above ID was found")
+		}
+
+		return res.status(200).send({message: "Job deleted successfully."})
+	} catch (error) {
+		console.log(error)
+		res.status(500).send("An error occurred")
+	}
+})
 export default router
